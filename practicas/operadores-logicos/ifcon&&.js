c3ac2573b978b5f3fedcc8dfaa96b59cc26b9871
DannyMarperOne <dannymarper.32@gmail.com>
2024-02-08 08:41:59 -0600

    Archivo ifcon&&

---
+++ practicas/operadores-logicos/ifcon&&.js
@@ -0,0 +1,25 @@
+// OPERADORES LÓGICOS
+
+
+// Ejercicio con AND && 
+// Escribe una condición “if” para comprobar que age(edad) está entre 14 y 90 inclusive.
+// “Inclusive” significa que age puede llegar a ser uno de los extremos, 14 o 90.
+let age = 100;
+if (age >= 14 && age <= 90) {
+    console.log('estas en la edad adecuada');
+}
+
+// Ejercicio con NOT !
+// Escribe una condición if para comprobar que age NO está entre 14 y 90 inclusive.
+// Crea dos variantes: la primera usando NOT !, y la segunda sin usarlo.
+let age2 = 100;
+if (!(age2 >= 14 && age2 <= 90)) {
+    console.log('No estás en el rango de edad deseado');
+}
+
+
+// Ejecicio con OR ||
+// Esto es una variante del ejercicio anterior, pero aqui utilizamos OR || 
+if (age2 < 14 || age2 > 90) {
+    console.log('No estás en el rango de edad deseado');
+}
