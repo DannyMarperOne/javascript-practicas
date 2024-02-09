dc77b7498c2b2a7161ba95f90ff9909e5b427340
DannyMarperOne <dannymarper.32@gmail.com>
2024-02-09 15:55:49 -0600

    Ejercicio de Operadores Lógicos

--- practicas-javascript/operadores-logicos/operadorlogico.js
+++ practicas-javascript/operadores-logicos/operadorlogico.js
@@ -1,10 +1,9 @@
 // OPERADORES LÓGICOS
 
-
 // Ejercicio con AND && 
 // Escribe una condición “if” para comprobar que age(edad) está entre 14 y 90 inclusive.
 // “Inclusive” significa que age puede llegar a ser uno de los extremos, 14 o 90.
-let age = 100;
+let age = 15;
 if (age >= 14 && age <= 90) {
     console.log('estas en la edad adecuada');
 }
@@ -17,8 +16,7 @@ if (!(age2 >= 14 && age2 <= 90)) {
     console.log('No estás en el rango de edad deseado');
 }
 
-
-// Ejecicio con OR ||
+// Ejercicio con OR ||
 // Esto es una variante del ejercicio anterior, pero aqui utilizamos OR || 
 if (age2 < 14 || age2 > 90) {
     console.log('No estás en el rango de edad deseado');
