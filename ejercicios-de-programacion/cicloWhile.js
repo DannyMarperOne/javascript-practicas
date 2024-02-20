17ce072ca7fa45c9576b7c6a36fa781f9c98c90e
DannyMarperOne <dannymarper.32@gmail.com>
2024-02-19 18:03:05 -0600

    Bucles y ejercicio de Numeros Primos

---
+++ ejercicios-de-programacion/cicloWhile.js
@@ -0,0 +1,16 @@
+/* Reescribe el código cambiando el bucle for a while sin alterar su comportamiento (la salida debería ser la misma). */
+
+/* 
+
+for (let i = 0; i < 3; i++) {
+  alert( `número ${i}!` );
+}
+
+*/
+
+
+let i = 0;
+while (i < 3) {
+    console.log(`número ${i}!`);
+    i++
+}
