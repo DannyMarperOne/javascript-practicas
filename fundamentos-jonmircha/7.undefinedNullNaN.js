3cbefe36bc4f152fb2a6004269be064dfb242116
DannyMarperOne <dannymarper.32@gmail.com>
2024-04-12 12:07:14 -0600

    Actualización de Archivo UndefindeNullNaN

--- fundamentos-jonmircha/7.undefinedNullNaN.js
+++ fundamentos-jonmircha/7.undefinedNullNaN.js
@@ -1,1 +1,29 @@
-// REVISAR NOTION
+/* 
+Undefined: 
+
+Para JavaScript es una variable que fue declarada pero no se le asignó 
+ningún valor, por tal cuando se imprime esa variable, JavaScript retorna Undefined. 
+En si Undefined significa que es una variable que no se ha inicializado y que tiene 
+ausencia de valor.
+*/
+
+let noDefinida;
+
+/* 
+Null: 
+
+En si null es una variable vacía, pero en este caso el que decide que sea vacía 
+es el programador y no JavaScript. Entonces el programador declara las variables como 
+null, o que es lo mismo ausencia de valor.
+*/
+
+let nulo = null;
+
+/* 
+NaN: 
+
+No es un número, esto sucede cuando se quiere realizar una operación aritmética entre un 
+número y un tipo de dato diferente a un número.
+*/
+
+let noEsUnNumero = 5 * "computadora";
