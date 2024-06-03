38547fd33f2393ce28c34a1e82d632b41a7f2d96
DannyMarperOne <dannymarper.32@gmail.com>
2024-06-03 09:57:12 -0600

    Estructuras de JS

--- retos-de-programacion-mouredev/03-ESTRUCTURAS DE DATOS.js
+++ retos-de-programacion-mouredev/03-ESTRUCTURAS DE DATOS.js
@@ -176,21 +176,6 @@ const myMap = new Map([
 myMap.set("Dios Egipcio", "Obelisco el atormetador"); //Agregar un elemento Key-Value
 console.log(myMap.delete("Trampa")); //Eliminar un elemento Key-Value
 console.log(myMap.get("Magica")); //Obtienes el Value asociado al Key
+console.log(myMap.size); //Verificar tamaño de Map
 
 
-
-
-const inventory = [
-    { name: 'asparagus', type: 'vegetables', quantity: 9 },
-    { name: 'bananas', type: 'fruit', quantity: 5 },
-    { name: 'goat', type: 'meat', quantity: 23 },
-    { name: 'cherries', type: 'fruit', quantity: 12 },
-    { name: 'fish', type: 'meat', quantity: 22 },
-];
-
-const restock = { restock: true };
-const sufficient = { restock: false };
-const result = Map.groupBy(inventory, ({ quantity }) =>
-    quantity < 6 ? restock : sufficient,
-);
-console.log(result.get(restock));
