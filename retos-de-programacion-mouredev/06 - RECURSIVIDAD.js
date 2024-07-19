34563577370d6671c34403d5b0badcb8615a62b7
DannyMarperOne <dannymarper.32@gmail.com>
2024-07-19 17:11:26 -0600

    Recursividad 02

--- retos-de-programacion-mouredev/06 - RECURSIVIDAD.js
+++ retos-de-programacion-mouredev/06 - RECURSIVIDAD.js
@@ -13,8 +13,7 @@
 
 */
 
-// Fibonacci
-
+// RECURSIVE FIBONACCI
 function fibonacci(n) {
     if (n <= 1) {
         return n
@@ -22,5 +21,16 @@ function fibonacci(n) {
         return fibonacci(n - 1) + fibonacci(n - 2)
     }
 }
-
 console.log(fibonacci(9));
+
+// RECURSIVE FIBONACCI
+const sequence = [];
+const iterations = 10;
+const fibonacciRecursive = (sequence) => {
+    sequence.length < 2 ? sequence.push(1) : sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+    if (sequence.length === iterations) {
+        return sequence.toString();
+    }
+    return fibonacciRecursive(sequence);
+};
+console.log(fibonacciRecursive(sequence));
